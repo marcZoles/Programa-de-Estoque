80cf23e12604c85f664028cdbf992757ffaaae41
Gabriel Marczal <2025106086@unibrasil.com.br>
2025-10-28 21:31:13 -0300

    Adicionando estruturas de pastas

---
+++ EstoqueConsole/src/Servico/CsvArmazenamento.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstoqueConsole.src.Servico
+{
+    internal class CsvArmazenamento
+    {
+    }
+}
